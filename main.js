7c613506ad13569a32a296779c6e97daf28959e0
Chris Legassick <chrislegassick@Chriss-Air.broadband>
2020-10-03 21:40:38 +0100

    Updated CSS and started JS functions

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const random = document.getElementById('random-btn');
+const output = document.getElementById('output');
+
+submit.addEventListener('submit', searchRecipe);
+random.addEventListener('click', getRandomRecipe);
+
+function searchRecipe(e) {
+  e.preventDefault();
+  const value = search.value;
+  fetch(`https://legassick-recipes.herokuapp.com/api/v1/recipes?name=${value}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
+}
+
+function getRandomRecipe() {
+  fetch('https://legassick-recipes.herokuapp.com/api/v1/recipes/random')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
+}
+
 const swiper = new Swiper('.swiper-container', {
   effect: 'coverflow',
   grabCursor: true,
