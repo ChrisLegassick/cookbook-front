1f5414830f19cb04facab5308f23559a1ba85fa7
Chris Legassick <chrislegassick@Chriss-Air.broadband>
2020-10-09 16:47:33 +0100

    Updated styles and added JS getAllRecipes on page load

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 const search = document.getElementById('search');
 const submit = document.getElementById('submit');
 const random = document.getElementById('random-btn');
-const output = document.getElementById('output');
+const recipeHeading = document.getElementById('recipe-heading');
+const recipeOutput = document.getElementById('recipe-output');
 
 submit.addEventListener('submit', searchRecipe);
 random.addEventListener('click', getRandomRecipe);
+
+function getAllRecipes() {
+  fetch('https://legassick-recipes.herokuapp.com/api/v1/recipes')
+    .then(res => res.json())
+    .then(data => {
+      const recipe = data.data;
+
+      recipeOutput.innerHTML = recipe
+        .map(
+          recipe => `
+          <div class="recipe swiper-slide" data-recipeID="${recipe._id}">
+            <p>${recipe.name}</p>
+          </div>
+        `
+        )
+        .join('');
+      initSwiper();
+    });
+}
+
+getAllRecipes();
 
 function searchRecipe(e) {
   e.preventDefault();
@@ -12,7 +34,24 @@ function searchRecipe(e) {
   fetch(`https://legassick-recipes.herokuapp.com/api/v1/recipes?name=${value}`)
     .then(res => res.json())
     .then(data => {
-      console.log(data);
+      const recipe = data.data;
+
+      if (recipe.length === 0) {
+        recipeHeading.innerHTML = `<p>No results found for "${value}"</p>`;
+      } else {
+        recipeHeading.innerHTML = `<p>${recipe.length} results found for "${value}"</p>`;
+
+        recipeOutput.innerHTML = recipe
+          .map(
+            recipe => `
+          <div class="recipe swiper-slide" data-recipeID="${recipe._id}">
+            <p>${recipe.name}</p>
+          </div>
+        `
+          )
+          .join('');
+      }
+      search.value = '';
     });
 }
 
@@ -24,17 +63,19 @@ function getRandomRecipe() {
     });
 }
 
-const swiper = new Swiper('.swiper-container', {
-  effect: 'coverflow',
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  loop: true,
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true
-  }
-});
+function initSwiper() {
+  const swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true
+    }
+  });
+}
